b2670e3e94bb18cad1646c1b644c35e4e17fef64
Luis Quishpi <luis.quishpi@gmail.com>
2023-06-05 21:00:31 -0500

    Init project

---
+++ src/main/java/ec/edu/insteclrg/DemoColaborativoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.insteclrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ec/edu/insteclrg/controller/v1/PaisController.java
@@ -0,0 +1,26 @@
+package ec.edu.insteclrg.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.insteclrg.dto.PaisDTO;
+import ec.edu.insteclrg.service.crud.PaisService;
+
+@RestController
+@RequestMapping("/api/v1.0/pais/")
+public class PaisController {
+
+	@Autowired
+	PaisService service;
+
+	@PostMapping
+	public ResponseEntity<Object> guardar(@RequestBody PaisDTO dto) {
+		service.save(dto);
+		return new ResponseEntity<>("Guardado correctamente", HttpStatus.OK);
+	}
+}
