14c33484bb4b65fa846fbfa593cebb9b5032ea24
Marco Verdugo <verdugomarco03@gmail.com>
2023-06-05 21:25:36 -0500

    Creada la clase Marco

---
+++ src/main/java/ec/edu/insteclrg/domain/Marco.java
@@ -0,0 +1,14 @@
+package ec.edu.insteclrg.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Marco {
+
+	private Long id;
+	private String nombre;
+}
