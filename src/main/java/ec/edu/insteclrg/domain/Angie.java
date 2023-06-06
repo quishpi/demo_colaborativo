2acc08665c019044af46dd9f005b652c1c35718e
Angie Vizhñay <paolaviz122333@gmail.com>
2023-06-05 21:25:17 -0500

    Crado la clase Angie

---
+++ src/main/java/ec/edu/insteclrg/domain/Angie.java
@@ -0,0 +1,15 @@
+package ec.edu.insteclrg.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Angie {
+	
+	private Long id;
+	private String Nombre;
+
+}
