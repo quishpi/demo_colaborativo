5a8f4a066dc5875705fbfe08666984b50e484aba
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-06-05 21:35:35 -0500

    creado  clase nube

--- src/main/java/ec/edu/insteclrg/domain/Nube.java
+++ src/main/java/ec/edu/insteclrg/domain/Nube.java
@@ -1,5 +1,14 @@
 package ec.edu.insteclrg.domain;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Nube {
+	private  Long id;
+	private  String nombre ;
 
 }
