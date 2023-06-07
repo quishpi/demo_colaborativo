b1c5002ed3d03bfc5cdb48eea615117bcbbda1f2
dome baquerizo <dome.baquerizo@insteclrg.edu.ec>
2023-06-06 21:10:29 -0500

    clase Provincia

---
+++ src/main/java/ec/edu/insteclrg/controller/v1/ProvinciaController.java
@@ -0,0 +1,26 @@
+package ec.edu.insteclrg.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import ec.edu.insteclrg.dto.ProvinciaDTO;
+import ec.edu.insteclrg.service.crud.ProvinciaService;
+
+@RestController
+@RequestMapping("api/v1.0/provincia/")
+public class ProvinciaController {
+	
+	@Autowired
+	ProvinciaService service;
+	
+	@PostMapping
+	public ResponseEntity<Object> guardar(@RequestBody ProvinciaDTO dto) {
+		service.save(dto);
+		return new ResponseEntity<>("Guardado correctamente", HttpStatus.OK);
+	}
+}
