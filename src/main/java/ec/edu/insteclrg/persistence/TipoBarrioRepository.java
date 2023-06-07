e5d2752278e133e251c0bc33d7e62c1509961f8e
erick <erick@erick-Inspiron-3593>
2023-06-06 21:20:14 -0500

    Add: Class TipoBarrio, DTO, Repository, Service, Controller

---
+++ src/main/java/ec/edu/insteclrg/persistence/TipoBarrioRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.insteclrg.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.insteclrg.domain.TipoBarrio;
+
+public interface TipoBarrioRepository extends JpaRepository<TipoBarrio , Long>{
+
+}
