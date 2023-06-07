7b86c676889f6c67c0f3b8b4b22fe0653095726a
Romulo Coronel <romulo.coronel@insteclrg.edu.ec>
2023-06-07 10:55:20 -0500

    delete class ciudad

--- src/main/java/ec/edu/insteclrg/persistence/CiudadRepository.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.insteclrg.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.edu.insteclrg.domain.Ciudad;
-public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
-
-}
