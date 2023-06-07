97d5b9871710e3cd0a95ee676af8fc30561e75a6
JONNATHAN SUMBA <JONNATHAN SUMBA@DESKTOP-JCH64DN>
2023-06-06 21:19:13 -0500

    Add: Class Casa,CasaDTO, CasaService, CasaRepository, CasaController

---
+++ src/main/java/ec/edu/insteclrg/domain/Casa.java
@@ -0,0 +1,35 @@
+package ec.edu.insteclrg.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Casa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String propietario;
+	
+	@Column(nullable = false, length = 50)
+	private String telefono;
+	
+	@Column(nullable = false, length = 50)
+	private String ubicacion;
+	
+	@Column(nullable = false, length = 50)
+	private String referencia;
+	
+	@Column(nullable = false, length = 50)
+	private String color;
+}
---
+++ src/main/java/ec/edu/insteclrg/dto/CasaDTO.java
@@ -0,0 +1,14 @@
+package ec.edu.insteclrg.dto;
+
+import lombok.Data;
+
+@Data
+public class CasaDTO {
+	
+	private Long id;
+	private String propietario;
+	private String telefono;
+	private String ubicacion;
+	private String color;
+	
+}
